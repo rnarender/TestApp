92c0db2e339f8935139d1ce73315dae55679b0c2
Narender Ramadheni <Narender.Ramadheni@Wolterskluwer.com>
2021-09-06 18:39:26 +0530

    Added Error handling in API and Client App

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,14 @@
+﻿
+namespace API.Errors;
+public class ApiException
+{
+    public ApiException(int statusCode, string message = null, string detials = null)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = detials;  
+    }
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public string Details { get; set; }
+}
